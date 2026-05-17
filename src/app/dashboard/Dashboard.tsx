0272fae3c847ae03ec22d08139b45048c4ebdfd8
Lee Donson <leesdev44@gmail.com>
2026-05-17 23:23:18 +1000

    update dashboard UI

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,15 +1,160 @@
 const Dashboard = () => {
   return (
-    <div className="flex flex-col items-center justify-center relative h-screen z-10">
-      <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
-
-      <p className="text-md  w-full md:w-1/2 mt-4 border border-slate-700 rounded-md p-2">
-        This page is still under development, data is temporarilly unavailable.
-        As soon as the site is updated, changes will be reflected immediately.
-      </p>
-      <p className="text-md md:hidden w-full md:w-1/2 mt-4 font-bold border border-slate-700 rounded-md p-2">
-        Please click the menu on the top right to see more pages.
-      </p>
+    <div className="flex-5 bg-[#000000] h-full overflow-y-auto rounded-lg p-2 border border-slate-800/50">
+      {/* main header */}
+      <div className="mt-20 border-b border-slate-700 mb-4">
+        <h2 className="text-2xl font-bold text-white mb-2">
+          Welcome Back, Inventor!
+        </h2>
+        <p className="text-slate-400 mb-4">
+          Here's a quick overview of your dashboard. Check out the latest stats
+          and updates on your projects, products, and ideas.
+        </p>
+      </div>
+      {/* main content */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {/* products */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Products
+            <span className="text-sm text-slate-400 ml-2">
+              +5% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+1,389</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                Sold Items: <span className="text-blue-600">1,389</span>{" "}
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                In Stock: <span className="text-green-500">389</span>
+              </div>
+              <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                Out of Stock: <span className="text-red-500">1,000</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* ideas */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Ideas
+            <span className="text-sm text-slate-400 ml-2">
+              +12% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+3,245</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                Approved: <span className="text-green-500">1,200</span>
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                Under Review: <span className="text-blue-500">1,500</span>
+              </div>
+              <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                Rejected: <span className="text-red-500">545</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* projects */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Projects
+            <span className="text-sm text-slate-400 ml-2">
+              +8% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+2,389</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                Completed: <span className="text-green-500">1,389</span>
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                In Progress: <span className="text-blue-500">389</span>
+              </div>
+              <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                Pending: <span className="text-red-500">1,000</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* revenue */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Revenue
+            <span className="text-sm text-slate-400 ml-2">
+              +20% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+$1.2M</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                Online: <span className="text-green-500">$800K</span>
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                In-store: <span className="text-blue-500">$400K</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* expense */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Expenses
+            <span className="text-sm text-slate-400 ml-2">
+              +10% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+$800K</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                Operational: <span className="text-green-500">$500K</span>
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                Marketing: <span className="text-blue-500">$300K</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* customers */}
+        <div className="visual-dashboard">
+          {/* header */}
+          <div className="flex items-center justify-between mb-2 p-2">
+            Total Customers
+            <span className="text-sm text-slate-400 ml-2">
+              +15% from last month
+            </span>
+          </div>
+          {/* content */}
+          <div className="p-2 text-white">
+            <p className="visual-text-dashboard">+5,000</p>
+            <div className="flex items-center gap-4 text-sm text-slate-400">
+              <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                New: <span className="text-green-500">3,000</span>
+              </div>
+              <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                Returning: <span className="text-blue-500">2,000</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/custom/hero-section.tsx
+++ src/components/custom/hero-section.tsx
@@ -1,9 +1,51 @@
 "use client";
 
 import { motion } from "motion/react";
-import { Lightbulb, ArrowRight, Box } from "lucide-react";
+import {
+  Lightbulb,
+  ArrowRight,
+  Box,
+  Blocks,
+  SquareChartGantt,
+  HandCoins,
+  CircleDollarSign,
+  Users,
+} from "lucide-react";
 import Footer from "@/app/Footer";
 import { useRouter } from "next/navigation";
+
+const dashboardTabs = [
+  {
+    label: "Products",
+    href: "/dashboard/products",
+    icon: <Box size={20} />,
+  },
+  {
+    label: "Ideas",
+    href: "/dashboard/ideas",
+    icon: <Lightbulb size={20} />,
+  },
+  {
+    label: "Projects",
+    href: "/dashboard/projects",
+    icon: <SquareChartGantt size={20} />,
+  },
+  {
+    label: "Revenue",
+    href: "/dashboard/revenue",
+    icon: <HandCoins size={20} />,
+  },
+  {
+    label: "Expense",
+    href: "/dashboard/expense",
+    icon: <CircleDollarSign size={20} />,
+  },
+  {
+    label: "Customers",
+    href: "/dashboard/customers",
+    icon: <Users size={20} />,
+  },
+];
 
 export const HeroSection = () => {
   const router = useRouter();
@@ -88,14 +130,151 @@ export const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.5, duration: 1 }}
-          className="mt-20 relative"
+          className="mt-15 relative"
         >
-          <div className="absolute inset-0 bg-linear-to-t from-[#030712] via-transparent to-transparent z-10" />
+          <div className="absolute inset-0 bg-linear-to-t from-[#030712] via-transparent to-transparent  z-20" />
           <div className="rounded-2xl border border-slate-800 bg-slate-900/40 p-4 backdrop-blur-sm">
-            <div className="h-64 md:h-96 w-full rounded-lg bg-[#0a0f1d] border border-slate-800/50 flex items-center justify-center">
-              <p className="text-slate-600 font-mono text-sm">
-                [ Project Visualization Interface ]
-              </p>
+            <div className="h-64 md:h-96 w-full rounded-lg bg-[#0a0f1d]  flex items-center justify-center gap-1">
+              <div className="flex-1 hidden md:block bg-neutral-800 border rounded-lg p-2 border-slate-800/50 h-full">
+                {/* header */}
+                <div className="flex items-center gap-2 mb-2">
+                  <Blocks className="h-5 w-5" color="blue" />
+                  <h2 className="text-sm font-bold text-white tracking-tight">
+                    Invent3 Lab
+                  </h2>
+                </div>
+                <p className="text-muted-foreground">Admin Dashboard.</p>
+                <div className="z-40 mt-4">
+                  {dashboardTabs.map((tab) => (
+                    <div
+                      key={tab.href}
+                      className="flex border border-slate-700 items-center gap-2 mt-4 p-2 hover rounded-md hover:bg-red-500 cursor-pointer transition-colors"
+                    >
+                      {tab.icon}
+                      <span className="text-sm text-slate-300">
+                        {tab.label}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="flex-5 bg-slate-800 h-full overflow-hidden rounded-lg p-2 border border-slate-800/50">
+                <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
+                  {/* products */}
+                  <div className="visual-dashboard">
+                    {/* header */}
+                    <div className="flex items-center justify-between mb-2 p-2">
+                      Total Products
+                      <span className="text-sm text-slate-400 ml-2">
+                        +5% from last month
+                      </span>
+                    </div>
+                    {/* content */}
+                    <div className="p-2 text-white">
+                      <p className="text-xl py-4 font-extrabold text-blue-600">
+                        +1,389
+                      </p>
+                      <div className="flex items-center gap-4 text-sm text-slate-400">
+                        <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                          Sold Items:{" "}
+                          <span className="text-blue-600">1,389</span>{" "}
+                        </div>
+                        <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                          In Stock: <span className="text-green-500">389</span>
+                        </div>
+                        <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                          Out of Stock:{" "}
+                          <span className="text-red-500">1,000</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* ideas */}
+                  <div className="visual-dashboard">
+                    {/* header */}
+                    <div className="flex items-center justify-between mb-2 p-2">
+                      Total Ideas
+                      <span className="text-sm text-slate-400 ml-2">
+                        +12% from last month
+                      </span>
+                    </div>
+                    {/* content */}
+                    <div className="p-2 text-white">
+                      <p className="text-xl py-4 font-extrabold text-blue-600">
+                        +3,245
+                      </p>
+                      <div className="flex items-center gap-4 text-sm text-slate-400">
+                        <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                          Approved:{" "}
+                          <span className="text-green-500">1,200</span>
+                        </div>
+                        <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                          Under Review:{" "}
+                          <span className="text-blue-500">1,500</span>
+                        </div>
+                        <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                          Rejected: <span className="text-red-500">545</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* projects */}
+                  <div className="visual-dashboard">
+                    {/* header */}
+                    <div className="flex items-center justify-between mb-2 p-2">
+                      Total Projects
+                      <span className="text-sm text-slate-400 ml-2">
+                        +8% from last month
+                      </span>
+                    </div>
+                    {/* content */}
+                    <div className="p-2 text-white">
+                      <p className="text-xl py-4 font-extrabold text-blue-600">
+                        +2,389
+                      </p>
+                      <div className="flex items-center gap-4 text-sm text-slate-400">
+                        <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                          Completed:{" "}
+                          <span className="text-green-500">1,389</span>
+                        </div>
+                        <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                          In Progress:{" "}
+                          <span className="text-blue-500">389</span>
+                        </div>
+                        <div className="bg-red-500/20 text-red-500 px-2 py-1 rounded-md">
+                          Pending: <span className="text-red-500">1,000</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* revenue */}
+                  <div className="visual-dashboard">
+                    {/* header */}
+                    <div className="flex items-center justify-between mb-2 p-2">
+                      Total Revenue
+                      <span className="text-sm text-slate-400 ml-2">
+                        +20% from last month
+                      </span>
+                    </div>
+                    {/* content */}
+                    <div className="p-2 text-white">
+                      <p className="text-xl py-4 font-extrabold text-blue-600">
+                        +$1.2M
+                      </p>
+                      <div className="flex items-center gap-4 text-sm text-slate-400">
+                        <div className="bg-green-500/20 text-green-500 px-2 py-1 rounded-md">
+                          Online: <span className="text-green-500">$800K</span>
+                        </div>
+                        <div className="bg-blue-500/20 text-blue-500 px-2 py-1 rounded-md">
+                          In-store: <span className="text-blue-500">$400K</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="visual-dashboard">expense</div>
+                  <div className="visual-dashboard">customers</div>
+                </div>
+              </div>
             </div>
           </div>
         </motion.div>
